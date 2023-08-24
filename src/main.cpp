abc3bcc6308730e25535ebe239f45a86f66a2852
ShereenKhoja <43831078+ShereenKhoja@users.noreply.github.com>
2023-08-24 19:27:46 +0000

    Wrote the code to calculate pay based on the number of hours worked and the hourly wage

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 //******************************************************************************
 // File name:   main.cpp
-// Author:      
-// Date:        
-// Class:       
-// Assignment:  
-// Purpose:    
-// Hours: 
+// Author:      WRITE YOUR NAME HERE
+// Date:        8/29/2023
+// Class:       CS150-XX (Replace XX with your section number)
+// Assignment:  Pay Calculator
+// Purpose:     Calculates the user's pay based on an hourly wage and the
+//              number of hours worked
+// Hours:       0.5
 //******************************************************************************
 
 #include <iostream>
@@ -13,6 +14,21 @@
 using namespace std;
 
 int main () {
+  double hours, rate, pay;
+
+  // Get number of hours worked
+  cout << "How many hours did you work? ";
+  cin >> hours;
+
+  // Get the hourly pay rate
+  cout << "How much did you get paid per hour? ";
+  cin >> rate;
+
+  // Calculate the pay
+  pay = hours * rate;
+
+  // Display the pay
+  cout << "You have earned $" << pay << endl;
 
   return EXIT_SUCCESS;
 }
